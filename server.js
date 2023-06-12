106c8f005c4bbab44c753349f934f7a1f179b4dd
32bitsret <62143351+32bitsret@users.noreply.github.com>
2023-06-12 10:22:16 +0100

    update with pong files

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const port = process.env.PORT || 8080
 const IP = '0.0.0.0'
 
 app.get('/', (req, res) => {
-  console.log('Yayx!');
+  console.log('Yayux!');
   res.send('Yay!')
 })
 
-app.get('/ping', (req, res) => {
-  console.log('Ping received!')
-  res.send('Ping received!')
+app.get('/pong', (req, res) => {
+  console.log('Pong received!');
+  res.send('Pong received!')
 })
 
 app.get('/pingpong', async (req, res) => {
   try {
     // Make a request to the Pong service
-    const pongResponse = await axios.get(`${process.env.PONG_SERVICE_FQDN}/pong`)
+    const pongResponse = await axios.get(`${process.env.PING_SERVICE_FQDN}/ping`)
     
     // Respond with the result
-    res.send(`Pong received! Pong service says: "${pongResponse.data}"`)
+    res.send(`Ping received! Ping service says: "${pongResponse.data}"`)
   } catch (error) {
-    console.error(`Failed to ping Pong service: ${error}`);
-    console.log(`Failed to ping Pong service. ${process.env.PONG_SERVICE_FQDN}`);
-    res.status(500).send(`Failed to ping Pong service. ${process.env.PONG_SERVICE_FQDN}`);
+    console.error(`Failed to ping Pong service: ${error}`) 
+    console.log(`Failed to ping Pong service. ${process.env.PING_SERVICE_FQDN}`);
+    res.status(500).send(`Failed to pong Ping service: ${process.env.PING_SERVICE_FQDN}`);
   }
 })
 
